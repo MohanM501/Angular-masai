3f4a45fad4de4c8c54fba3260f5818d120ef4995
MohanM501 <mohananna501b@gmail.com>
2023-07-09 22:42:24 +0530

    added day-6 folder

--- Day-5/steps/projects/step-1-directives/src/app/app.component.ts
+++ Day-5/steps/projects/step-1-directives/src/app/app.component.ts
@@ -4,29 +4,72 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
+    <div >
       <h1>
-        Welcome to {{title}}!
+         directives in angular
       </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+     
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+    <h2>*ngFor directive </h2>
+    <div>
+      <p *ngFor="let hero of avengers; index as idx; first as fst; last as lst; odd as od; even as ev">
+      {{idx+1}} ~ 
+          <span *ngIf="fst"> First Avengers: </span>
+          <span *ngIf="lst"> Last Avengers: </span>
+          <span *ngIf="!fst && !lst">In between Avengers: </span>
+          <span *ngIf="od">Odd Number:-</span>
+          <span *ngIf="ev">Even Number:- </span>
+      {{hero}}
+      
+      </p>
+    </div>
+    <p>ngFor (ng-template) (no element loop on single line)</p>
+   <ng-template ngFor let-hero [ngForOf]="avengers">
+      {{hero}}
+   </ng-template>
+   <h2>*ngSwitch directive </h2>
+   <input #rng type="range" min="0" max="4" [value]="4" (input)="rateing=rng.value"/>{{rateing}}
+   <p>Rating</p>
+   <div [ngSwitch]="rateing">
+      <h3 *ngSwitchCase="1" class="rateing" >*</h3>
+      <h3 *ngSwitchCase="2" class="rateing" >**</h3>
+      <h3 *ngSwitchCase="3" class="rateing" >***</h3>
+      <h3 *ngSwitchCase="4" class="rateing" >****</h3>
+      <h3 *ngSwitchCase="5" class="rateing" >*****</h3>
+      <h3 *ngSwitchDefault class="rateing"> Not rated </h3>
+   </div>
+   <h4>Ng NonBindable </h4>
+   <p ngNonBindable>{{hi}}</p>
+
+   <h4>Ng Style Directive </h4>
+   <p [style]="styleprop">style prop</p>
+    <h4 [ngStyle]="{'bgcolor':bgCol2,'color':'darkblue'}">show color based on condition</h4>
+   <h4 [ngStyle]="{'background-color':rateing<2 ? 'yellow':'red'}"> Rating Power </h4>
+   <h4>Ng Class (preffered than ngStye)</h4>
+   <p [ngClass]="{redBox:rateing>1,blueBox:rateing>3}">Ng clllass</p>
+   `,
+   styles:[`
+     .rateing{
+        margin:0px;
+        font-family:arial;
+        color:goldenrod;
+     }
+     
+     .blueBox{
+      width:20px;
+       background-color:blue;
+     }
+     .redBox{
+       width:20px;
+       background-color:red;
+     }
+   `]
+  
 })
 export class AppComponent {
   title = 'step-1-directives';
+  avengers=["batman","superman","antman","Thor","GreenMan"];
+  rateing:any=4;
+  styleprop="border:2px solid red";
+  bgCol2="blue";
 }
